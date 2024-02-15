104e61d3f40bd2855b6e3622d1285d725cc111bd
JayQkew <rayjebb@gmail.com>
2024-02-15 16:41:12 +0200

    camera shake

---
+++ Light Weight!!!/Assets/_Scripts/Managers/CameraManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance { get; private set; }
+
+    public CinemachineVirtualCamera VirtualCamera;
+
+    public float cameraShakeIncrement = 0.01f;
+    public float cameraShakeDecrease = 0.007f;
+
+    public float amplitureShake;
+    public float frequencyShake;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        InputManager.Instance.mainMashEvent.AddListener(CameraShakeIncrease);
+    }
+
+    private void Update()
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = VirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        frequencyShake += cameraShakeDecrease * Time.deltaTime;
+        amplitureShake += cameraShakeDecrease * Time.deltaTime;
+
+        amplitureShake = Mathf.Clamp(amplitureShake, -0.3f, 0);
+        frequencyShake = Mathf.Clamp(frequencyShake, -0.3f, 0);
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = amplitureShake;
+        cinemachineBasicMultiChannelPerlin.m_FrequencyGain = frequencyShake;
+    }
+
+    public void CameraShakeIncrease()
+    {
+        frequencyShake -= cameraShakeIncrement;
+        amplitureShake -= cameraShakeIncrement;
+    }
+}
--- Light Weight!!!/Assets/_Scripts/Managers/ExerciseManager.cs
+++ Light Weight!!!/Assets/_Scripts/Managers/ExerciseManager.cs
@@ -11,26 +11,8 @@ public class ExerciseManager : MonoBehaviour
 
 
     [Header("Right/Centre")]
-    
-    //maximum change per increment
-    //how fast the weight drops
-    //can depend on stamina and weight lifted
-    public float repRate_R = 1f;         
-    
-    public float currentRepCount_R;
-    
-    //change depending on weight
-    //this is the weight the player is lifting
-    public float weight_R = 1f;     
-    
-    //increment at each press
-    //this increases the more the players muscles grow
+    public float strength_Both;
     public float strength_R;    
-
-    [Header("Left")]
-    public float repRate_L = 1f;         
-    public float currentRepCount_L;
-    public float weight_L = 1f;     
     public float strength_L;    
 
     public GameObject barbell;
@@ -44,9 +26,6 @@ public class ExerciseManager : MonoBehaviour
     private void Start()
     {
         barbell_rb = barbell.GetComponent<Rigidbody2D>();
-
-        currentRepCount_R = weight_R;
-        currentRepCount_L = weight_L;
 
         // set weight and min/max positions
         switch (currentMashType)
@@ -77,27 +56,8 @@ public class ExerciseManager : MonoBehaviour
 
     public void PushRep_L()
     {
-        currentRepCount_L += strength_L;
-        currentRepCount_L = Mathf.Clamp(currentRepCount_L, 0, weight_L);
+        barbell_rb.AddForce(Vector2.up * strength_L, ForceMode2D.Impulse);
     }
-
-    public void MashUpdate()
-    {
-        switch (currentMashType)
-        {
-            case MashType.Squat:
-                barbell.transform.position = new Vector3(barbell.transform.position.x, Mathf.MoveTowards(barbell.transform.position.y, 0, Mathf.Sqrt(repRate_R) * Time.deltaTime));
-                break;
-            case MashType.Deadlift:
-                barbell.transform.position = new Vector3(barbell.transform.position.x, Mathf.MoveTowards(barbell.transform.position.y, 0, Mathf.Sqrt(repRate_R) * Time.deltaTime));
-                break;
-            case MashType.Bench:
-                currentRepCount_R = Mathf.MoveTowards(currentRepCount_R, 0, Mathf.Sqrt(repRate_R) * Time.deltaTime);
-                currentRepCount_L = Mathf.MoveTowards(currentRepCount_L, 0, Mathf.Sqrt(repRate_L) * Time.deltaTime);
-                break;
-        }
-    }
-
 }
 
 public enum MashType
